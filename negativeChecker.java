3b5cd5d922dfa8402bc3fc0ac3cef2d8c26b755e
Anurag <anuragsingh3@outlook.com>
2020-06-11 17:59:04 +0800

    Optimisation.

--- negativeChecker.java
+++ negativeChecker.java
@@ -1,16 +1,16 @@
 public class negativeChecker {
-	
+
 	/*
 	*	PURPOSE: determine if the imported value is a negative number
 	*
 	*	IMPORT: integer value to be checked
 	*
-	*	RETURN: if the import value is negative it will return true, 
+	*	RETURN: if the import value is negative it will return true,
 	*	otherwise it will return false
 	*/
 	public boolean isNegative (int var) {
 
-		if (var * 2 < var) {
+		if (var < 0) {
 			return true;
 		}
 		return false;
